0484abbde25ea4fe89a4a899c850528c700c90d5
xiaoxian <none@none>
2018-04-23 00:33:12 +0800

    add ass06_02 and its docs

--- Chapter06_pointer/Ass06_02/ass06_02.cpp
+++ Chapter06_pointer/Ass06_02/ass06_02.cpp
@@ -5,38 +5,62 @@
 
 using namespace std;
 
-void sub_str_creator(int n, int m, string &str, vector<string> &str_vector){
-	string temp_str;
+void sub_str_creator(string &str, int* combine, int n, int m, const int M, vector<string> &str_vector){
+	if(m == 0){
+		str_vector.push_back("");
+		return;
+	}
+	for(int i = n; i>=m; i--){
+		combine[m -1] = i - 1;
+		if(m > 1){
+			sub_str_creator(str, combine, i-1, m-1, M, str_vector);
+		}
+		else{
+			string str_temp;
+			for(int j = M-1; j >= 0; j--){
+				str_temp += str[combine[j]];
+			}
+			str_vector.push_back(str_temp);
+		}
+	}
 }
 
 int main(){
 	string str;
+	vector<string> str_vector;
 	int count = 0;
 	getline(cin,str);
 
-	cout << "The input string is: " << str << ". Length is: " << str.length() << endl;
+	//cout << "The input string is: " << str << ". Length is: " << str.length() << endl;
 
-	for(int i = 0; i <= str.length(); i++){
-		vector<string> str_vector;
-		sub_str_creator(str.length(), i, str, str_vector);		//计算子字符串
+	for(int m = 0; m <= str.length(); m++){
+		//cout << "----------Loop: " << m << "------------" << endl;
+		str_vector.clear();
+		//cout << "The size of string vector is: " << str_vector.size() << endl;
+		int* combine = new int[m];
+		sub_str_creator(str, combine, str.length(), m, m, str_vector);		//计算子字符串
+		int i = 0;
 		for(const auto s : str_vector){
 			string sub_str_inv;
-			cout << "Current sub string is: " << s << endl;
-			cout << "The length of sub string is: " << s.length() << endl;
+			//cout << "The length of sub string is: " << s.length() << endl;
 			if(s.empty()){
-				cout << "Sub string is empty" << endl;
+				//cout << "Sub string is empty" << endl;
 				continue;
 			}
 			//if(!sub_str.empty()) cout << "false" << endl;
 			for(int k = s.length()-1; k >= 0; k--){		//回文子字符串
 				sub_str_inv += s[k];
 			}
-			cout << "The inversed sub string is: " << sub_str_inv << endl;
+			//cout << " comb" << i << "----------------" << endl;
+			//cout << "Current sub string is: " << s << ". ";
+			//cout << "The inversed sub string is: " << sub_str_inv << endl;
 			if(s == sub_str_inv){
 				count++;
 			}
-			cout << "The num is: " << count << endl;
+			i++;
+			//cout << "The num is: " << count << endl;
 		}
+		delete combine;
 	}
 	
 	cout << count << endl;
