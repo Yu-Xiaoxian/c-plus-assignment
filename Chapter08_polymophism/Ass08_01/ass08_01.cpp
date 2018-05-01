be60a33c4766d56e1cabde83fb98d4c6fa0afe96
xiaoxian <self@yu-xiaoxian.me>
2018-05-01 18:20:53 +0800

    add ass08_01 and docs

--- Chapter08_polymophism/Ass08_01/ass08_01.cpp
+++ Chapter08_polymophism/Ass08_01/ass08_01.cpp
@@ -1,7 +1,55 @@
+#include <cstdio>
+#include <cstring>
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
-int main(){
-	return 0;
+class Complex {
+public:
+	Complex(double r = 0.0, double i = 0.0) : real(r), imag(i) {};
+	Complex operator+ (const Complex &c2) const;
+	Complex operator- (const Complex &c2) const;
+
+	/*实现下面三个函数*/
+	Complex operator* (const Complex &c2) const;
+	Complex operator/ (const Complex &c2) const;
+	friend ostream & operator<< (ostream &out, const Complex &c);
+
+private:
+	double real;
+	double imag;
+};
+
+Complex Complex::operator+ (const Complex &c2) const {
+	return Complex(real + c2.real, imag + c2.imag);
+}
+
+Complex Complex::operator- (const Complex &c2) const {
+	return Complex(real - c2.real, imag - c2.imag);
+}
+
+Complex Complex::operator* (const Complex &c2) const {
+	return Complex(real * c2.real - imag * c2.imag, real * c2.imag + imag * c2.real);
+}
+
+Complex Complex::operator/ (const Complex &c2) const {
+	return Complex((real * c2.real + imag * c2.imag)/ (c2.real* c2.real + c2.imag * c2.imag), (- real * c2.imag + imag * c2.real)/(c2.real * c2.real + c2.imag * c2.imag));
+}
+
+ostream & operator<< (ostream &out, const Complex &c) {
+	out << c.real << " " << c.imag << endl;
+	return out;
+}
+
+int main() {
+	double real, imag;
+	cin >> real >> imag;
+	Complex c1(real, imag);
+	cin >> real >> imag;
+	Complex c2(real, imag);
+	cout << c1 + c2;
+	cout << c1 - c2;
+	cout << c1 * c2;
+	cout << c1 / c2;
 }
--- Chapter08_polymophism/Ass08_02/ass08_02.cpp
+++ Chapter08_polymophism/Ass08_02/ass08_02.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
-
 using namespace std;
 
-int main(){
-	return 0;
+const double pi = 3.14;
+
+class Shape {
+public:
+	Shape() {}
+	~Shape() {}
+	virtual double getArea() = 0;
+	virtual double getPerim() = 0;
+};
+
+class Circle : public Shape {
+public:
+	Circle(double rad) :radius(rad) {}
+	~Circle() {}
+
+	/*补充这两个函数*/
+	double getArea();
+	double getPerim();
+private:
+	double radius;
+};
+
+int main() {
+	double radius;
+	cin >> radius;
+	Circle c(radius);
+	cout << c.getArea() << " " << c.getPerim() << endl;
 }
