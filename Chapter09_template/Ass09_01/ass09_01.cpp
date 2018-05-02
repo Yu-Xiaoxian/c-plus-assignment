36b18ab3f711f0a1b20225f3b4a193235263ce7c
xiaoxian <self@yu-xiaoxian.me>
2018-05-02 17:54:52 +0800

    update code and docs of Chapter 09

--- Chapter09_template/Ass09_01/ass09_01.cpp
+++ Chapter09_template/Ass09_01/ass09_01.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
-
 using namespace std;
 
-int main(){
+/*请在这里填充代码*/
+template<typename T>
+T getSum(T *arr, int size) {
+	T sum = 0;
+	for (int i = 0; i < size; i++) {
+		sum += arr[i];
+	}
+	return sum;
+}
+
+int main()
+{
+	int n, m;
+	cin >> n >> m;
+	int* arr_int = new int[n];
+	double* arr_double = new double[m];
+	for (int i = 0; i < n; ++i)
+		cin >> arr_int[i];
+	for (int i = 0; i < m; ++i)
+		cin >> arr_double[i];
+	cout << getSum(arr_int, n) << endl;
+	cout << getSum(arr_double, m) << endl;
 	return 0;
 }
--- Chapter09_template/Ass09_02/ass09_02.cpp
+++ Chapter09_template/Ass09_02/ass09_02.cpp
@@ -1,7 +1,41 @@
 #include <iostream>
-
 using namespace std;
 
-int main(){
+template <class T>
+int binSearch(T arr[], int n, T key);
+
+/*请实现折半查找，并按顺序输出被访问的元素下标*/
+template <class T>
+int binSearch(T arr[], int n, T key) {
+	int low, high, mid;
+	low = 0;
+	high = n - 1;
+	while (low <= high) {
+		mid = (int)((low + high) / 2);
+		cout << mid << endl;
+		if (arr[mid] == key)
+			return mid;
+		else if (arr[mid] < key)
+			low = mid + 1;
+		else
+			high = mid - 1;
+	}
+		return -1;
+}
+
+int main()
+{
+	int n, m;
+	int key1;
+	double key2;
+	cin >> n >> m >> key1 >> key2;
+	int* arr_int = new int[n];
+	double* arr_double = new double[m];
+	for (int i = 0; i < n; ++i)
+		cin >> arr_int[i];
+	for (int i = 0; i < m; ++i)
+		cin >> arr_double[i];
+	binSearch(arr_int, n, key1);
+	binSearch(arr_double, m, key2);
 	return 0;
 }
