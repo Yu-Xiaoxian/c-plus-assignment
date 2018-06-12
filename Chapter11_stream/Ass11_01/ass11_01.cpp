1307e7e5be715e837d1fe1da5ef776245b539567
xiaoxian <self@yu-xiaoxian.me>
2018-06-12 23:32:56 +0800

    update code of assigment 09-02

--- Chapter11_stream/Ass11_01/ass11_01.cpp
+++ Chapter11_stream/Ass11_01/ass11_01.cpp
@@ -1,7 +1,34 @@
 #include <iostream>
-
+#include <vector>
+#include <string>
+#include <algorithm>
 using namespace std;
 
-int main(){
+class Student
+{
+public:
+	int id;
+	string name, grade;
+	Student() {};
+	Student(int id, string name, string grade);
+	bool operator < (const Student & s) const;
+};
+
+istream & operator >> (istream & in, Student & s);
+ostream & operator << (ostream & out, Student & s);
+
+/*请在这里填充代码*/
+
+int main()
+{
+	vector<Student> sv;
+	Student temp;
+	while (cin >> temp)
+	{
+		sv.push_back(temp);
+	}
+	sort(sv.begin(), sv.end());
+	for (int i = 0; i < sv.size(); ++i)
+		cout << sv[i];
 	return 0;
 }
