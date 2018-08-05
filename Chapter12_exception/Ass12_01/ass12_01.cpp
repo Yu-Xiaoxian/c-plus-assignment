2f83e0b4d592853231d3ebfbb9f391f8eaaa3299
xiaoxian <self@yu-xiaoxian.me>
2018-08-05 15:16:19 +0800

    add source of Chapter 12 Exception

--- Chapter12_exception/Ass12_01/ass12_01.cpp
+++ Chapter12_exception/Ass12_01/ass12_01.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
+#include <stdexcept>
+#include <cmath>
+#include <iomanip>
 
 using namespace std;
 
-int main(){
-	return 0;
+/*ÍêÉÆ´Ëº¯Êý*/
+double calArea(double a, double b, double c) {
+	if(a < 0 || b < 0 || c < 0)
+		throw invalid_argument("The input is illegal");
+	if (a + b <= c || a + c <= b || c + b <= a)
+		throw invalid_argument("The input is illegal");
+	if (a != b && b != c && a != c)
+		throw invalid_argument("The input is illegal");
+	else {
+		double p = (a + b + c) / 2;
+		double area = sqrt(p * (p - a)*(p - b)*(p - c));
+		return area;
+	}
+}
+
+int main() {
+	double a, b, c;
+	cin >> a >> b >> c;
+	try {
+		double area = calArea(a, b, c);
+		cout << fixed << setprecision(2) << area << endl;
+	}
+	catch (exception& e) {
+		cout << e.what() << endl;
+	}
 }
--- Chapter12_exception/README.md
+++ Chapter12_exception/README.md
@@ -1,3 +1,8 @@
 # Assignments of Chapter XII
 
 This chapter is about exception handling.
+
+## Ass12_01
+题目：输入三角形三边的边长，使用异常处理模块，判断是否为等边三角形：如果是，则输出面积；如果不是，则抛出异常。
+
+解答：在calArea函数中使用if语句进行判断，然后抛出异常。交给catch块去处理。
